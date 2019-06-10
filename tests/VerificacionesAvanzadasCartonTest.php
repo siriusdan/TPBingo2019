d2ea1f5d7088a7c6389c9a93751326c0cdbf314f
siriusdan <50678649+siriusdan@users.noreply.github.com>
2019-06-10 09:51:07 -0300

    Update VerificacionesAvanzadasCartonTest.php

--- tests/VerificacionesAvanzadasCartonTest.php
+++ tests/VerificacionesAvanzadasCartonTest.php
@@ -6,12 +6,32 @@ use PHPUnit\Framework\TestCase;
 /** hola **/
 
 class VerificacionesAvanzadasCartonTest extends TestCase {
-
+  /**
+   * Funcion que posibilita el test "testUnoNoventa" para la clase CartonEjemplo.
+   */
+ public function rangoAceptable(){
+   $max = 0;
+   $min = 100;
+   foreach($this-> numerosDelCarton() as $num){
+     if($num < $min){
+       $min = $num;
+     }
+     if($num > $max){
+       $max = $num;
+     }
+   }
+   if($min > 0 && $max < 100){
+     return True;
+   } else {
+     return False;
+   }
+ }
   /**
    * Verifica que los números del carton se encuentren en el rango 1 a 90.
    */
   public function testUnoANoventa() {
-    $this->assertTrue(TRUE);
+    $carton = new CartonEjemplo;
+    $this->assertTrue(True, $carton->rangoAceptable());
   }
 
   /**
