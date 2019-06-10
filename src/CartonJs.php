28c25b23f1bc0506b0b0e5fe538aaa90ffdde8be
siriusdan <50678649+siriusdan@users.noreply.github.com>
2019-06-10 10:10:45 -0300

    Update CartonJs.php

--- src/CartonJs.php
+++ src/CartonJs.php
@@ -22,11 +22,7 @@ class CartonJs implements CartonInterface {
    * {@inheritdoc}
    */
   public function filas() {
-    return [
-      [4,0,24,31,40,0,0,0,80],
-      [0,13,0,39,48,0,66,72,0],
-      [1,0,27,0,0,50,0,73,86],
-    ];
+    return $this -> numeros_carton;
   }
 
   /**
