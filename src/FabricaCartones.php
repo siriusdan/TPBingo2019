91ac400a238b350cc6173716694ab8229d6c20a4
siriusdan <50678649+siriusdan@users.noreply.github.com>
2019-06-11 17:18:43 -0300

    Update FabricaCartones.php

--- src/FabricaCartones.php
+++ src/FabricaCartones.php
@@ -6,13 +6,13 @@ class FabricaCartones {
 
   public function generarCarton() {
     // Algo de pseudo-código para ayudar con la evaluacion.
-    $carton = $this->intentoCarton();
-
-    if ($this->cartonEsValido($carton)) {
-      return $carton;
-    }
-  }
-
+    while(1){
+        $carton = $this->intentoCarton();
+        if ($this->cartonEsValido($carton)) {
+          return $carton;
+        }
+      }
+     }
   protected function cartonEsValido($carton) {
     if (validarUnoANoventa($carton) &&
       validarCincoNumerosPorFila($carton) &&
