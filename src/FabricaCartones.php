38822b0de2ac1c92079035e46cbacbf0bc6d6d9f
Bruno Formichella <51272441+BrunoFormichella@users.noreply.github.com>
2019-06-11 23:18:20 -0300

    Update FabricaCartones.php

--- src/FabricaCartones.php
+++ src/FabricaCartones.php
@@ -6,7 +6,7 @@ class FabricaCartones {
 
   public function generarCarton() {
     for($i=0;$i<20;$i++){
-        $carton = new Carton($this->intentoCarton());
+        $carton = $this->intentoCarton();
         if ($this->cartonEsValido($carton)) {
           return $carton;
         }
